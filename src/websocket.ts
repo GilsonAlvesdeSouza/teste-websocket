c7a973590e7f48bef62aa13eb897ca589109a882
Gilson Alves De Souza <gilsonalvesdesouza@hotmail.com>
2022-11-23 17:11:58 -0300

    adicionado cronometro

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,14 +1,57 @@
 import { Request, Response } from "express";
 import { io, app } from "./http";
 
-app.get("/msg", (req: Request, res: Response) => {
-  let count = 0;
+let segundos = 0;
+let minutos = 0;
+let tempo = 0;
+let pausar = false;
+
+const contadorTempo = () => {
+  const idTempo = setInterval(() => {
+    tempo++;
+    let minutoAtual = Math.floor(tempo / 59);
+    minutos = minutoAtual;
+    io.emit("seg", { segundos, minutos });
+    console.log(tempo);
+    segundos++;
+    if (segundos === 60) {
+      segundos = 0;
+    }
+    if (pausar) {
+      console.log("entrou");
+      clearInterval(idTempo);
+      io.emit("seg", { segundos, minutos });
+    }
+  }, 100);
+};
+
+app.get("/iniciar", (req: Request, res: Response) => {
+  if (tempo <= 0) {
+    contadorTempo();
+  }
   res.redirect("http://localhost:3000");
-  setInterval(() => {
-    io.emit("msg", count);
-    console.log(count);
-    count++;
-  }, 1000);
+});
+
+app.get("/pausar", (req: Request, res: Response) => {
+  pausar = true;
+  io.emit("seg", { segundos, minutos });
+  res.redirect("http://localhost:3000");
+});
+
+app.get("/continuar", (req: Request, res: Response) => {
+  if (pausar) {
+    pausar = false;
+    contadorTempo();
+  }
+  res.redirect("http://localhost:3000");
+});
+
+app.get("/zerar", (req: Request, res: Response) => {
+  segundos = 0;
+  minutos = 0;
+  tempo = 0;
+  pausar = false;
+  res.redirect("http://localhost:3000");
 });
 
 io.on("connection", (socket) => {
